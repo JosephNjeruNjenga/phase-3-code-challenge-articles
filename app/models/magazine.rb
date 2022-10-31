cdef45c5e0bb1ac5019b9014f32293ea45ded911
+ <+>
2022-10-31 11:51:39 +0300

    Completed Challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,33 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
-
-
+  def self.all
+    @@all
+  end
+  def articles
+    Article.all.filter {|article| article.magazine.name == @name}
+  end
+  def authors
+    articles.map{|art|Author.find_by_name(art.author.name)}
+  end
+  def contributors
+    authors.uniq
+  end
+  def self.find_by_name(name)
+    @@all.find {|mag| mag.name == name}
+  end
+  def article_titles
+    articles.map {|art| art.title}
+  end
+  def contributing_authors
+    busy_authors = []
+    authors.tally.each {|key, value| value > 2 ? busy_authors << key : nil}
+    busy_authors
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+joe = Author.new("Joe")
+mag1 = Magazine.new("Life", "Lifestyle")
+art1 = Article.new(joe, mag1, "Life Lessons Pt. 1")
+art2 = Article.new(joe, mag1, "Life Lessons Pt. 2")
+lynn = Author.new("Lynn")
+art3 = Article.new(lynn, mag1, "Exploring Life")
+art4 = Article.new(lynn, mag1, "Condescending Life")
+mag2 = Magazine.new("Dream", "Drama")
+art5 = Article.new(joe, mag2, "How to")
+art6 = Article.new(joe, mag1, "Life Lessons Pt. 3")
 
 
 
